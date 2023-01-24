cbb011b4b8e09d99795f6977d68e71b9c95550d5
Mohammodjuwelislam <mohammodjuwel61@gmail.com>
2023-01-24 18:46:29 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,185 @@
+const mobileMenuToggle = document.querySelector('.mobileMenuToggle');
+const mobileMenuArea = document.querySelector('.mobile-menu-area');
+const closeToggle = document.querySelector('.closeToggle');
+
+mobileMenuToggle.addEventListener('click', function(){
+	mobileMenuArea.classList.add('show');
+	console.log(mobileMenuArea);
+});
+
+closeToggle.addEventListener('click', function(){
+	mobileMenuArea.classList.remove('show');
+	console.log(mobileMenuArea);
+});
+
+
+
+(function ($) {
+"use strict";
+
+
+
+
+$('.slider-active').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+	nav: true,
+    responsive:{
+        0:{
+            items:2,
+            navText:['<i class="fa-solid fa-arrow-left-long"></i>','<i class="fa-solid fa-arrow-right-long"></i>'],
+    		nav:true,
+			dots:true
+        },
+        600:{
+            items:2,
+            navText:['<i class="fa-solid fa-arrow-left-long"></i>','<i class="fa-solid fa-arrow-right-long"></i>'],
+    		nav:true,
+			dots:true
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:true,
+			navText:['<i class="fa-solid fa-arrow-left-long"></i>','<i class="fa-solid fa-arrow-right-long"></i>'],
+    		nav:true,
+			dots:true
+        }
+    }
+});
+
+
+
+
+
+
+// product-active 
+$('.product-active ').owlCarousel({
+    loop:true,
+    margin:30,
+    responsiveClass:true,
+	smartSpeed: 2000,
+	nav: true,
+    responsive:{
+        0:{
+            items:1,
+            navText:['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+    		nav:true,
+			dots:false,
+        },
+        600:{
+            items:1,
+            navText:['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+    		nav:false,
+			dots:false,
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true,
+			navText:['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+    		nav:false,
+			dots:false,
+        }
+    }
+});
+
+
+
+
+
+
+
+$('.features-active').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    margin: 15,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+            slidesToShow:2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+            slidesToShow:2,
+        }
+      }
+    ]
+});
+
+
+// scrollToTop
+$.scrollUp({
+	scrollName: 'scrollUp', // Element ID
+	topDistance: '300', // Distance from top before showing element (px)
+	topSpeed: 300, // Speed back to top (ms)
+	animation: 'fade', // Fade, slide, none
+	animationInSpeed: 200, // Animation in speed (ms)
+	animationOutSpeed: 200, // Animation out speed (ms)
+	scrollText: '<i class="icofont icofont-long-arrow-up"></i>', // Text for element
+	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+});
+
+// WOW active
+new WOW().init();
+
+
+
+
+
+//splide.blog-active
+splide = new Splide('.splide.blog-active', {
+    gap: '15px',
+    arrows: false,
+    perPage: 1,
+    padding:{right:'500px'},
+    type: 'loop',
+    pagination: false,
+    keyboard: false,  // Splide listens to key events at the document level and moves ALL carousels when arrow keys are used. Also, keyboard controls are not expected by real users.
+    slideFocus: false,  // removes tabindex="0" from each slide wrapper, since we only want our links inside each slide to receive focus.  
+    
+    breakpoints: {
+      1400: {
+        perPage: 1,
+        padding: {
+            right: '500px'
+        },
+      },
+      1200: {
+        perPage: 1,
+        padding: {
+            right: '400px'
+        },
+      },
+      800: {
+        perPage: 1,
+        padding: {
+            right: '300px'
+        },
+      },
+      600: {
+        perPage: 1,
+        padding: {
+            right: '100px'
+        },
+      }
+    }
+}).mount();
+
+
+
+
+
+
+
+
+
+})(jQuery);
